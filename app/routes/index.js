853b8d1be435338667e368a9ce723a12ad0c6fba
Ivan5 <converse_ivan@hotmail.com>
2018-06-16 23:10:34 -0500

    Add some basic routes

---
+++ app/routes/index.js
@@ -0,0 +1,5 @@
+const noteRoutes = require('./notes-routes');
+
+module.exports = function(app,db){
+  noteRoutes(app,db);
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8000;
+
+require('./app/routes')(app,{});
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+  console.log('Server started');
+  
+})
